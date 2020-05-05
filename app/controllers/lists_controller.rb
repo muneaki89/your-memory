f9bdf90b29ddc6a22550dfd3f8c752ea9ef183c6
muneaki <muneaki89@gmail.com>
2020-05-05 22:44:37 +0900

    _

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,4 +1,9 @@
 class ListsController < ApplicationController
+  
   def index
   end
+
+  def show
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   root "lists#index"
   resources :users, only: [:edit, :update]
+  resources :lists, only: [:show]
 end
